f5c06a579acccc842acfce4af2448494cfc773ea
wilwe <wilwe036@gmail.com>
2025-01-24 07:48:58 +0100

    how

--- src/main/java/com/github/wilwe21/gsad/datagen/ModRecipeProvider.java
+++ src/main/java/com/github/wilwe21/gsad/datagen/ModRecipeProvider.java
@@ -1,26 +1,24 @@
 package com.github.wilwe21.gsad.datagen;
 
+import com.github.wilwe21.gsad.Gsad;
 import com.github.wilwe21.gsad.block.ModBlock;
-import com.github.wilwe21.gsad.item.ItemTags;
 import com.github.wilwe21.gsad.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.data.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.ShapedRecipe;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -35,15 +33,20 @@ public class ModRecipeProvider extends FabricRecipeProvider {
             @Override
             public void generate() {
                 offerReversibleCompactingRecipes(RecipeCategory.MISC, ModItems.STARDUST, RecipeCategory.MISC, ModBlock.DREAM_BLOCK);
-//                this.createShaped(RecipeCategory.FOOD, ModItems.GRAVICE, 1)
-//                        .input("#", Items.GRAVEL)
-//                        .input("|", Items.STICK)
+                this.createShaped(RecipeCategory.FOOD, ModItems.GRAVICE, 1)
+                        .input('#', Items.GRAVEL)
+                        .input('|', Items.STICK)
+                        .pattern("#")
+                        .pattern("#")
+                        .pattern("|")
+                        .offerTo(this.exporter);
             }
         };
     }
 
-    @Override
+        @Override
     public String getName() {
         return "gsad";
     }
 }
+
