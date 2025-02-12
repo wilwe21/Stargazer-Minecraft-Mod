b045de3bbbcc8c3d61cfbbe1adef4e80f1fdd6cc
wilwe <wilwe036@gmail.com>
2025-02-12 17:11:20 +0100

    audio fix

--- src/main/java/com/github/wilwe21/stargazer/sound/SoundGroups.java
+++ src/main/java/com/github/wilwe21/stargazer/sound/SoundGroups.java
@@ -3,10 +3,13 @@ package com.github.wilwe21.stargazer.sound;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvents;
 
+import java.util.Random;
+
 public class SoundGroups {
+    private static final Random random = new Random();
     public static final BlockSoundGroup STAR = new BlockSoundGroup(
-            0.4F,
-            1.4F,
+            0.1F,
+            1.5F + random.nextFloat(0.2F),
             SoundEffects.BLOCK_COSMIC_BREAK,
             SoundEvents.INTENTIONALLY_EMPTY,
             SoundEffects.BLOCK_COSMIC_PLACE,
