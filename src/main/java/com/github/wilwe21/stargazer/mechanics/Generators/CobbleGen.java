230d8e753bcbd740b53471cfd5fb535362d51f0d
wilwe <wilwe036@gmail.com>
2025-02-25 21:55:59 +0100

    ok it's work

--- src/main/java/com/github/wilwe21/stargazer/mechanics/Generators/CobbleGen.java
+++ src/main/java/com/github/wilwe21/stargazer/mechanics/Generators/CobbleGen.java
@@ -12,38 +12,38 @@ import static net.minecraft.block.FluidBlock.FLOW_DIRECTIONS;
 
 public class CobbleGen {
     public CobbleGen(Block lava, Block Obs, Block Cobble) {
-        this.LAVA = lava;
-        this.BLOCKCOBBLE = Cobble.getDefaultState();
-        this.BLOCKOBS = Obs.getDefaultState();
+        LAVA = lava;
+        BLOCKCOBBLE = Cobble.getDefaultState();
+        BLOCKOBS = Obs.getDefaultState();
     }
     public CobbleGen(Block lava, BlockState Obs, Block Cobble) {
-        this.LAVA = lava;
-        this.BLOCKCOBBLE = Cobble.getDefaultState();
-        this.BLOCKOBS = Obs;
+        LAVA = lava;
+        BLOCKCOBBLE = Cobble.getDefaultState();
+        BLOCKOBS = Obs;
     }
     public CobbleGen(Block lava, Block Obs, BlockState Cobble) {
-        this.LAVA = lava;
-        this.BLOCKCOBBLE = Cobble;
-        this.BLOCKOBS = Obs.getDefaultState();
+        LAVA = lava;
+        BLOCKCOBBLE = Cobble;
+        BLOCKOBS = Obs.getDefaultState();
     }
     public CobbleGen(Block lava, BlockState Obs, BlockState Cobble) {
-        this.LAVA = lava;
-        this.BLOCKCOBBLE = Cobble;
-        this.BLOCKOBS = Obs;
+        LAVA = lava;
+        BLOCKCOBBLE = Cobble;
+        BLOCKOBS = Obs;
     }
-    private static Block LAVA;
-    private static BlockState BLOCKOBS;
-    private static BlockState BLOCKCOBBLE;
+    protected final Block LAVA;
+    protected final BlockState BLOCKOBS;
+    protected final BlockState BLOCKCOBBLE;
 
     public boolean gen(World world, BlockPos pos) {
         for (Direction direction : FLOW_DIRECTIONS) {
-            if (Helpers.isColliding(this.LAVA, world, pos, direction)) {
+            if (Helpers.isColliding(LAVA, world, pos, direction)) {
                 BlockPos blockPos = pos.offset(direction.getOpposite());
                 if (world.getFluidState(blockPos).isIn(FluidTags.WATER)) {
-                    BlockState block = world.getFluidState(pos).isStill() ? this.BLOCKOBS : this.BLOCKCOBBLE;
+                    BlockState block = world.getFluidState(pos).isStill() ? BLOCKOBS : BLOCKCOBBLE;
                     if (direction == Direction.DOWN) {
                         BlockPos newPos = Helpers.getCollidingPosition(LAVA, world, pos, FLOW_DIRECTIONS);
-                        world.setBlockState(newPos, this.BLOCKOBS);
+                        world.setBlockState(newPos, BLOCKOBS);
                     } else {
                         world.setBlockState(pos, block);
                     }
@@ -52,9 +52,9 @@ public class CobbleGen {
             }
         }
         if (world.getFluidState(pos).isStill()) {
-            if (Helpers.isCollidingAny(this.LAVA, world, pos, FLOW_DIRECTIONS)) {
-                BlockState block = world.getFluidState(pos).isStill() ? this.BLOCKOBS : this.BLOCKCOBBLE;
-                BlockPos newPos = Helpers.getCollidingPosition(this.LAVA, world, pos, FLOW_DIRECTIONS);
+            if (Helpers.isCollidingAny(LAVA, world, pos, FLOW_DIRECTIONS)) {
+                BlockState block = world.getFluidState(pos).isStill() ? BLOCKOBS : BLOCKCOBBLE;
+                BlockPos newPos = Helpers.getCollidingPosition(LAVA, world, pos, FLOW_DIRECTIONS);
                 world.setBlockState(newPos, block);
                 return false;
             }
