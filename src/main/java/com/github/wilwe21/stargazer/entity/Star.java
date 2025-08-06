68745d1a7912cfd3336b965933d5b52dc7a8e3d0
wilwe <wilwe036@gmail.com>
2025-08-06 08:19:09 +0200

    some fixes

--- src/main/java/com/github/wilwe21/stargazer/entity/Star.java
+++ src/main/java/com/github/wilwe21/stargazer/entity/Star.java
@@ -1,30 +1,9 @@
 package com.github.wilwe21.stargazer.entity;
 
-import net.minecraft.block.BlockState;
+import com.github.wilwe21.stargazer.item.ModItems;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.control.FlightMoveControl;
-import net.minecraft.entity.ai.pathing.BirdNavigation;
-import net.minecraft.entity.ai.pathing.EntityNavigation;
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.FlyingEntity;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.PigEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.vehicle.BoatEntity;
-import net.minecraft.entity.vehicle.VehicleEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.entity.vehicle.AbstractBoatEntity;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -35,16 +14,13 @@ import software.bernie.geckolib.animation.PlayState;
 import software.bernie.geckolib.animation.RawAnimation;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class Star extends VehicleEntity implements GeoEntity, Leashable {
+public class Star extends AbstractBoatEntity implements GeoEntity {
     protected static final RawAnimation ROTATO = RawAnimation.begin().thenLoop("star.rotate");
-
-    @Nullable
-    private Leashable.LeashData leashData;
 
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
 
     public Star(EntityType<? extends Star> type, World world) {
-        super(type, world);
+        super(type, world, () -> ModItems.YELLOW_STAR);
     }
 
     @Override
@@ -62,50 +38,7 @@ public class Star extends VehicleEntity implements GeoEntity, Leashable {
     }
 
     @Override
-    protected Entity.MoveEffect getMoveEffect() {
-        return MoveEffect.EVENTS;
-    }
-
-    @Override
-    public ActionResult interact(PlayerEntity player, Hand hand) {
-        ActionResult actionResult = super.interact(player, hand);
-        if (actionResult != ActionResult.PASS) {
-            return actionResult;
-        }
-        if (!player.shouldCancelInteraction() && (this.getWorld().isClient || player.startRiding(this))) {
-            return ActionResult.SUCCESS;
-        }
-        return ActionResult.PASS;
-    }
-
-    @Override
-    protected void readCustomDataFromNbt(NbtCompound nbt) {
-
-    }
-
-    @Override
-    protected void writeCustomDataToNbt(NbtCompound nbt) {
-
-    }
-
-    @Override
-    protected Item asItem() {
-        return null;
-    }
-
-    @Override
-    @Nullable
-    public Leashable.LeashData getLeashData() {
-        return this.leashData;
-    }
-
-    @Override
-    public void setLeashData(@Nullable Leashable.LeashData leashData) {
-        this.leashData = leashData;
-    }
-
-    @Override
-    public Vec3d getLeashOffset() {
-        return new Vec3d(0.0, 0.88f * this.getStandingEyeHeight(), this.getWidth() * 0.64f);
+    protected double getPassengerAttachmentY(EntityDimensions dimensions) {
+        return 0;
     }
 }
